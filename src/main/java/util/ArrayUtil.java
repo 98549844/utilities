1ed324880912354a2531e39053284f9a5ff3df9e
garlam <garlam_au@qq.com>
2021-07-31 01:18:32 +0800

    update

---
+++ src/main/java/util/ArrayUtil.java
@@ -0,0 +1,19 @@
+package util;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * @Classname: ArrayUtil
+ * @Date: 31/7/2021 1:14 上午
+ * @Author: garlam
+ * @Description:
+ */
+
+public class ArrayUtil {
+    private static Logger log = LogManager.getLogger(ArrayUtil.class.getName());
+
+
+
+}
+
