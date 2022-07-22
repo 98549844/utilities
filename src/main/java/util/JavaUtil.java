dce3098a7c90b1cd765666bb83de3697238fe3e8
Kalam <kalam_au@kinetix.com>
2022-07-23 00:04:04 +0800

    update

--- src/main/java/util/JavaUtil.java
+++ src/main/java/util/JavaUtil.java
@@ -44,45 +44,45 @@ public class JavaUtil {
 	}
 
 
-	public static void getJavaInfo() {
-		Console.print("Java版本号:\t", Console.BLUE);
-		Console.println(System.getProperty("java.version"), Console.BLACK); // java版本号
-		Console.print("Java提供商名称:\t", Console.BLUE);
-		Console.println(System.getProperty("java.vendor"), Console.BLACK); // Java提供商名称
-		Console.print("Java提供商网站:\t", Console.BLUE);
-		Console.println(System.getProperty("java.vendor.url"), Console.BLACK); // Java提供商网站
-		Console.print("jre目录:\t", Console.BLUE);
-		Console.println(System.getProperty("java.home"), Console.BLACK); // Java，哦，应该是jre目录
-		Console.print("Java虚拟机规范版本号:\t", Console.BLUE);
-		Console.println(System.getProperty("java.vm.specification.version"), Console.BLACK); // Java虚拟机规范版本号
-		Console.print("Java虚拟机规范提供商:\t", Console.BLUE);
-		Console.println(System.getProperty("java.vm.specification.vendor"), Console.BLACK); // Java虚拟机规范提供商
-		Console.print("Java虚拟机规范名称:\t", Console.BLUE);
-		Console.println(System.getProperty("java.vm.specification.name"), Console.BLACK); // Java虚拟机规范名称
-		Console.print("Java虚拟机版本号:\t", Console.BLUE);
-		Console.println(System.getProperty("java.vm.version"), Console.BLACK); // Java虚拟机版本号
-		Console.print("Java虚拟机提供商:\t", Console.BLUE);
-		Console.println(System.getProperty("java.vm.vendor"), Console.BLACK); // Java虚拟机提供商
-		Console.print("Java虚拟机名称:\t", Console.BLUE);
-		Console.println(System.getProperty("java.vm.name"), Console.BLACK); // Java虚拟机名称
-		Console.print("Java规范版本号:\t", Console.BLUE);
-		Console.println(System.getProperty("java.specification.version"), Console.BLACK); // Java规范版本号
-		Console.print("Java规范提供商:\t", Console.BLUE);
-		Console.println(System.getProperty("java.specification.vendor"), Console.BLACK); // Java规范提供商
-		Console.print("Java规范名称:\t", Console.BLUE);
-		Console.println(System.getProperty("java.specification.name"), Console.BLACK); // Java规范名称
-		Console.print("Java类版本号:\t", Console.BLUE);
-		Console.println(System.getProperty("java.class.version"), Console.BLACK); // Java类版本号
-		Console.print("Java类路径:\t", Console.BLUE);
-		Console.println(System.getProperty("java.class.path"), Console.BLACK); // Java类路径
-		Console.print("Java lib路径:\t", Console.BLUE);
-		Console.println(System.getProperty("java.library.path"), Console.BLACK); // Java lib路径
-		Console.print("Java输入输出临时路径:\t", Console.BLUE);
-		Console.println(System.getProperty("java.io.tmpdir"), Console.BLACK); // Java输入输出临时路径
-		Console.print("Java编译器:\t", Console.BLUE);
-		Console.println(System.getProperty("java.compiler"), Console.BLACK); // Java编译器
-		Console.print("Java执行路径:\t", Console.BLUE);
-		Console.println(System.getProperty("java.ext.dirs"), Console.BLACK); // Java执行路径
+    public static void getJavaInfo() {
+        Console.print("Java版本号: \t", Console.BLUE);
+        Console.println(System.getProperty("java.version"), Console.BLACK); // java版本号
+        Console.print("Java提供商名称: \t", Console.BLUE);
+        Console.println(System.getProperty("java.vendor"), Console.BLACK); // Java提供商名称
+        Console.print("Java提供商网站: \t", Console.BLUE);
+        Console.println(System.getProperty("java.vendor.url"), Console.BLACK); // Java提供商网站
+        Console.print("jre目录: \t", Console.BLUE);
+        Console.println(System.getProperty("java.home"), Console.BLACK); // Java，哦，应该是jre目录
+        Console.print("Java虚拟机规范版本号: \t", Console.BLUE);
+        Console.println(System.getProperty("java.vm.specification.version"), Console.BLACK); // Java虚拟机规范版本号
+        Console.print("Java虚拟机规范提供商: \t", Console.BLUE);
+        Console.println(System.getProperty("java.vm.specification.vendor"), Console.BLACK); // Java虚拟机规范提供商
+        Console.print("Java虚拟机规范名称: \t", Console.BLUE);
+        Console.println(System.getProperty("java.vm.specification.name"), Console.BLACK); // Java虚拟机规范名称
+        Console.print("Java虚拟机版本号: \t", Console.BLUE);
+        Console.println(System.getProperty("java.vm.version"), Console.BLACK); // Java虚拟机版本号
+        Console.print("Java虚拟机提供商: \t", Console.BLUE);
+        Console.println(System.getProperty("java.vm.vendor"), Console.BLACK); // Java虚拟机提供商
+        Console.print("Java虚拟机名称: \t", Console.BLUE);
+        Console.println(System.getProperty("java.vm.name"), Console.BLACK); // Java虚拟机名称
+        Console.print("Java规范版本号: \t", Console.BLUE);
+        Console.println(System.getProperty("java.specification.version"), Console.BLACK); // Java规范版本号
+        Console.print("Java规范提供商: \t", Console.BLUE);
+        Console.println(System.getProperty("java.specification.vendor"), Console.BLACK); // Java规范提供商
+        Console.print("Java规范名称: \t", Console.BLUE);
+        Console.println(System.getProperty("java.specification.name"), Console.BLACK); // Java规范名称
+        Console.print("Java类版本号: \t", Console.BLUE);
+        Console.println(System.getProperty("java.class.version"), Console.BLACK); // Java类版本号
+        Console.print("Java类路径: \t", Console.BLUE);
+        Console.println(System.getProperty("java.class.path"), Console.BLACK); // Java类路径
+        Console.print("Java lib路径: \t", Console.BLUE);
+        Console.println(System.getProperty("java.library.path"), Console.BLACK); // Java lib路径
+        Console.print("Java输入输出临时路径: \t", Console.BLUE);
+        Console.println(System.getProperty("java.io.tmpdir"), Console.BLACK); // Java输入输出临时路径
+        Console.print("Java编译器: \t", Console.BLUE);
+        Console.println(System.getProperty("java.compiler"), Console.BLACK); // Java编译器
+        Console.print("Java执行路径: \t", Console.BLUE);
+        Console.println(System.getProperty("java.ext.dirs"), Console.BLACK); // Java执行路径
 
 	}
 }
