59d9c1518b00972aa835c3aad1ffb9808b6e1c23
Kalam <kalam_au@kinetix.com>
2023-05-24 18:02:41 +0800

    update

---
+++ src/main/java/com/tools/FileContentTool.java
@@ -0,0 +1,45 @@
+package com.tools;
+
+import com.util.FileUtil;
+import com.util.NullUtil;
+import com.util.StringUtil;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * @Classname: FileContentTool
+ * @Date: 2023/5/24 下午 04:54
+ * @Author: kalam_au
+ * @Description:
+ */
+
+
+public class FileContentTool {
+    private static final Logger log = LogManager.getLogger(FileContentTool.class.getName());
+
+    static final String sourcePath = "C:\\ideaPorject\\ace\\src\\main\\java\\com\\ace\\";
+    static final String sourceFile = "AceApplication.java";
+
+    public static void main(String[] args) throws Exception {
+        FileContentTool fileContentTool = new FileContentTool();
+        fileContentTool.replaceContentSymbol();
+    }
+
+    private void replaceContentSymbol() throws IOException {
+        String content = (String) Objects.requireNonNull(FileUtil.read(sourcePath + sourceFile)).get(FileUtil.ORIGINAL);
+        String result1 = content.replaceAll("，", ", ");
+        String result2 = result1.replaceAll("。", ". ");
+        String result3 = result2.replaceAll("：", ": ");
+        String result4 = result3.replaceAll("！", "! ");
+        String result0 = result4.replaceAll("；", "; ");
+
+        FileUtil.write(sourcePath, sourceFile, result0, false);
+    }
+
+}
+
